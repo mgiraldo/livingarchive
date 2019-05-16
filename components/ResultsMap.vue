320c5a3c194149e3c4b76d708c01d36750032633
Mauricio Giraldo <mauricio@pingpongestudio.com>
2019-05-15 22:11:16 -0400

    :space_invader: componentized/refactored map

---
+++ components/ResultsMap.vue
@@ -0,0 +1,227 @@
+<template>
+  <section ref="pane" class="map">
+    <no-ssr>
+      <l-map ref="map">
+        <l-tile-layer
+          v-for="(layer, index) in tilelayers"
+          :key="index"
+          :url="layer.url"
+          :options="layer.options"
+        ></l-tile-layer>
+        <l-control-scale
+          position="topright"
+          :imperial="false"
+          :metric="true"
+        ></l-control-scale>
+        <l-control class-name="legend" position="bottomleft">
+          <div class="legend">
+            <strong>{{ legendType }}</strong>
+            <ul class="legend-list">
+              <li
+                v-for="(color, name, index) in legend"
+                :key="index"
+                class="legend-list-item"
+              >
+                <filter-color-item :name="name" :color="color" />
+              </li>
+            </ul>
+            <button
+              v-if="legendType === 'sex'"
+              class="legend-toggle"
+              @click="toggleLegend"
+            >
+              color by age
+            </button>
+            <button
+              v-if="legendType === 'age'"
+              class="legend-toggle"
+              @click="toggleLegend"
+            >
+              color by sex
+            </button>
+          </div>
+        </l-control>
+        <l-marker
+          v-for="(individual, index) in individuals"
+          :key="index"
+          :lat-lng="individual.point.coordinates"
+          :rise-on-hover="true"
+        >
+          <l-icon class-name="icon">
+            <map-marker :type="legendType" :individual="individual" />
+          </l-icon>
+          <l-popup max-width="15rem" class="popup">
+            <dl>
+              <dt>Identifier</dt>
+              <dd>
+                <nuxt-link
+                  :to="`/skeleton/${individual.identifier}`"
+                  target="_blank"
+                  >{{ individual.identifier }}</nuxt-link
+                >
+              </dd>
+              <dt>Skeleton</dt>
+              <dd class="bones">
+                <bones-find-view :shape="individual.skeleton" />
+              </dd>
+              <dt>Sex</dt>
+              <dd>
+                {{ individual.sex }}
+                <button type="button" :value="individual.sex" @click="onlySex">
+                  ONLY
+                </button>
+              </dd>
+              <dt>Age</dt>
+              <dd>
+                {{ individual.age }}
+                <button type="button" :value="individual.age" @click="onlyAge">
+                  ONLY
+                </button>
+              </dd>
+            </dl>
+          </l-popup>
+        </l-marker>
+      </l-map>
+    </no-ssr>
+  </section>
+</template>
+
+<script>
+import { TILELAYERS } from '~/utils/constants'
+
+import MapMarker from '~/components/MapMarker'
+import FilterColorItem from '~/components/FilterColorItem'
+import BonesFindView from '~/components/BonesFindView'
+
+export default {
+  components: {
+    MapMarker,
+    FilterColorItem,
+    BonesFindView
+  },
+  props: {
+    individuals: { type: Object, required: true }
+  },
+  data() {
+    return {
+      tilelayers: TILELAYERS
+    }
+  },
+  computed: {
+    legendType() {
+      return this.$store.state.legendType
+    },
+    legend() {
+      if (this.$store.state.legendType === 'age') {
+        return this.$store.state.ages
+      } else {
+        return this.$store.state.sexes
+      }
+    }
+  },
+  mounted() {
+    this.$store.subscribe(mutation => {
+      if (mutation.type === 'fetchedIndividuals') {
+        this.fitMap()
+      }
+    })
+    this.checkMapObject()
+  },
+  methods: {
+    onlySex(event) {
+      this.onlyProp('sex', event.target.value)
+    },
+    onlyAge(event) {
+      this.onlyProp('age', event.target.value)
+    },
+    onlyProp(prop, value) {
+      this.$store.commit('onlyProp', { prop, value })
+      this.$store.dispatch('fetchIndividuals')
+    },
+    toggleLegend() {
+      this.$store.commit('toggledLegend')
+    },
+    fitMap() {
+      if (this.$refs.map && this.$store.state.points.length > 0) {
+        this.$refs.map.mapObject.fitBounds(this.$store.state.points)
+      }
+    },
+    checkMapObject() {
+      this.checkMap = setInterval(() => {
+        if (this.$refs.map) {
+          clearInterval(this.checkMap)
+          this.fitMap()
+        }
+      }, 100)
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.map {
+  flex-basis: 50%;
+}
+.legend {
+  background-color: transparentize($global-background-color, 0.25);
+  border-radius: 0.5rem;
+  list-style-type: none;
+  padding: 0.25rem;
+  text-transform: uppercase;
+
+  ul {
+    list-style-type: none;
+    margin: 0.5rem 0;
+    padding: 0;
+  }
+
+  .legend-list-item {
+    margin-bottom: 0.5rem;
+  }
+
+  .legend-list-item:last-child {
+    margin-bottom: 0;
+  }
+
+  .dot {
+    border-radius: 50%;
+    height: 1rem;
+    margin-right: 0.25rem;
+    width: 1rem;
+  }
+}
+.popup {
+  dt {
+    font-weight: bold;
+  }
+  dd {
+    margin-bottom: 0.5rem;
+  }
+  .bones {
+    height: 15rem;
+    width: 15rem;
+  }
+  .discussion {
+    max-height: 5rem;
+    overflow-y: auto;
+  }
+}
+</style>
+<style lang="scss">
+/* overriding/customising leaflet css is unscoped */
+.icon {
+  border-radius: 50%;
+  box-shadow: 0 0 0.1rem $global-background-color;
+}
+.leaflet-popup-content-wrapper {
+  background-color: $global-background-color;
+  color: $global-text-color;
+
+  a {
+    color: $global-link-color;
+  }
+}
+.leaflet-popup-tip {
+  background-color: $global-background-color;
+}
+</style>
--- pages/map/_state.vue
+++ pages/map/_state.vue
@@ -38,113 +38,17 @@
         </table>
       </section>
       <div class="resizer" @mousedown="resizeDown">⋮</div>
-      <section ref="mapPane" class="map">
-        <no-ssr>
-          <l-map ref="myMap">
-            <l-tile-layer
-              v-for="(layer, index) in tilelayers"
-              :key="index"
-              :url="layer.url"
-              :options="layer.options"
-            ></l-tile-layer>
-            <l-control-scale
-              position="topright"
-              :imperial="false"
-              :metric="true"
-            ></l-control-scale>
-            <l-control class-name="legend" position="bottomleft">
-              <div class="legend">
-                <strong>{{ legendType }}</strong>
-                <ul class="legend-list">
-                  <li
-                    v-for="(color, name, index) in legend"
-                    :key="index"
-                    class="legend-list-item"
-                  >
-                    <filter-color-item :name="name" :color="color" />
-                  </li>
-                </ul>
-                <button
-                  v-if="legendType === 'sex'"
-                  class="legend-toggle"
-                  @click="toggleLegend"
-                >
-                  color by age
-                </button>
-                <button
-                  v-if="legendType === 'age'"
-                  class="legend-toggle"
-                  @click="toggleLegend"
-                >
-                  color by sex
-                </button>
-              </div>
-            </l-control>
-            <l-marker
-              v-for="(individual, index) in individuals"
-              :key="index"
-              :lat-lng="individual.point.coordinates"
-              :rise-on-hover="true"
-            >
-              <l-icon class-name="icon">
-                <map-marker :type="legendType" :individual="individual" />
-              </l-icon>
-              <l-popup max-width="15rem" class="popup">
-                <dl>
-                  <dt>Identifier</dt>
-                  <dd>
-                    <nuxt-link
-                      :to="`/skeleton/${individual.identifier}`"
-                      target="_blank"
-                      >{{ individual.identifier }}</nuxt-link
-                    >
-                  </dd>
-                  <dt>Skeleton</dt>
-                  <dd class="bones">
-                    <bones-find-view :shape="individual.skeleton" />
-                  </dd>
-                  <dt>Sex</dt>
-                  <dd>
-                    {{ individual.sex }}
-                    <button
-                      type="button"
-                      :value="individual.sex"
-                      @click="onlySex"
-                    >
-                      ONLY
-                    </button>
-                  </dd>
-                  <dt>Age</dt>
-                  <dd>
-                    {{ individual.age }}
-                    <button
-                      type="button"
-                      :value="individual.age"
-                      @click="onlyAge"
-                    >
-                      ONLY
-                    </button>
-                  </dd>
-                </dl>
-              </l-popup>
-            </l-marker>
-          </l-map>
-        </no-ssr>
-      </section>
+      <results-map ref="mapPane" :individuals="individuals" />
     </div>
   </div>
 </template>
 
 <script>
-import { TILELAYERS } from '~/utils/constants'
-
 import { parseParams } from '~/utils/params'
 
 import SearchControls from '~/components/SearchControls'
 import ResultCell from '~/components/ResultCell'
-import MapMarker from '~/components/MapMarker'
-import FilterColorItem from '~/components/FilterColorItem'
-import BonesFindView from '~/components/BonesFindView'
+import ResultsMap from '~/components/ResultsMap'
 
 export default {
   head() {
@@ -154,16 +58,13 @@ export default {
   components: {
     SearchControls,
     ResultCell,
-    MapMarker,
-    FilterColorItem,
-    BonesFindView
+    ResultsMap
   },
   data() {
     return {
       resizing: false,
       splitPaneWidth: 0,
       splitPaneX: 0,
-      tilelayers: TILELAYERS,
       ageFilter: [...this.$store.state.checkedAges],
       sexFilter: [...this.$store.state.checkedSexes]
     }
@@ -177,29 +78,13 @@ export default {
     },
     individuals() {
       return this.$store.state.individuals
-    },
-    ages() {
-      return this.$store.state.ages
-    },
-    sexes() {
-      return this.$store.state.sexes
-    },
-    legendType() {
-      return this.$store.state.legendType
-    },
-    legend() {
-      if (this.$store.state.legendType === 'age') {
-        return this.$store.state.ages
-      } else {
-        return this.$store.state.sexes
-      }
     }
   },
   beforeRouteUpdate(to, from, next) {
     // console.log('route change', to, from, next)
     next()
   },
-  fetch: async function({ store, params, req }) {
+  fetch: async function({ store, params }) {
     // console.log('fetch')
     if (params.state) {
       // console.log('ssr map.fetch', params)
@@ -210,10 +95,8 @@ export default {
   },
   created() {},
   mounted() {
+    this.checkResizer()
     this.$store.subscribe(mutation => {
-      if (mutation.type === 'fetchedIndividuals') {
-        this.fitMap()
-      }
       if (
         mutation.type === 'setFilters' ||
         mutation.type === 'clearFilters' ||
@@ -223,9 +106,16 @@ export default {
         this.updateFilters()
       }
     })
-    this.checkMapObject()
   },
   methods: {
+    checkResizer() {
+      this.checkPane = setInterval(() => {
+        if (this.$refs.splitPane) {
+          clearInterval(this.checkPane)
+          this.pane = this.$refs.splitPane
+        }
+      }, 100)
+    },
     resizeDown() {
       this.resizing = true
       this.splitPaneWidth = this.pane.offsetWidth
@@ -238,13 +128,13 @@ export default {
     resizeMove(e) {
       if (this.resizing) {
         let left = this.$refs.resultsPane
-        let right = this.$refs.mapPane
+        let right = this.$refs.mapPane.$refs.pane
         let newX = e.clientX
         let pct = ((newX - this.splitPaneX) / this.splitPaneWidth) * 100
         pct = pct > 0 ? pct : 0
         left.style.flexBasis = pct + '%'
         right.style.flexBasis = 100 - pct + '%'
-        this.$refs.myMap.mapObject.invalidateSize()
+        this.$refs.mapPane.$refs.map.mapObject.invalidateSize()
       }
       e.preventDefault()
       e.stopPropagation()
@@ -259,38 +149,6 @@ export default {
     updateFilters() {
       this.ageFilter = [...this.$store.state.checkedAges]
       this.sexFilter = [...this.$store.state.checkedSexes]
-    },
-    onlySex(event) {
-      this.onlyProp('sex', event.target.value)
-    },
-    onlyAge(event) {
-      this.onlyProp('age', event.target.value)
-    },
-    onlyProp(prop, value) {
-      this.$store.commit('onlyProp', { prop, value })
-      this.$store.dispatch('fetchIndividuals')
-    },
-    toggleLegend() {
-      this.$store.commit('toggledLegend')
-    },
-    fitMap() {
-      if (this.$refs.myMap && this.$store.state.points.length > 0) {
-        this.$refs.myMap.mapObject.fitBounds(this.$store.state.points)
-      }
-    },
-    checkMapObject() {
-      this.checkPane = setInterval(() => {
-        if (this.$refs.splitPane) {
-          clearInterval(this.checkPane)
-          this.pane = this.$refs.splitPane
-        }
-      }, 100)
-      this.checkMap = setInterval(() => {
-        if (this.$refs.myMap) {
-          clearInterval(this.checkMap)
-          this.fitMap()
-        }
-      }, 100)
     }
   }
 }
@@ -335,70 +193,5 @@ export default {
   &:hover {
     background-color: lighten($global-background-color, 5%);
   }
-}
-.map {
-  flex-basis: 50%;
-}
-.legend {
-  background-color: transparentize($global-background-color, 0.25);
-  border-radius: 0.5rem;
-  list-style-type: none;
-  padding: 0.25rem;
-  text-transform: uppercase;
-
-  ul {
-    list-style-type: none;
-    margin: 0.5rem 0;
-    padding: 0;
-  }
-
-  .legend-list-item {
-    margin-bottom: 0.5rem;
-  }
-
-  .legend-list-item:last-child {
-    margin-bottom: 0;
-  }
-
-  .dot {
-    border-radius: 50%;
-    height: 1rem;
-    margin-right: 0.25rem;
-    width: 1rem;
-  }
-}
-.popup {
-  dt {
-    font-weight: bold;
-  }
-  dd {
-    margin-bottom: 0.5rem;
-  }
-  .bones {
-    height: 15rem;
-    width: 15rem;
-  }
-  .discussion {
-    max-height: 5rem;
-    overflow-y: auto;
-  }
-}
-</style>
-<style lang="scss">
-/* overriding/customising leaflet css is unscoped */
-.icon {
-  border-radius: 50%;
-  box-shadow: 0 0 0.1rem $global-background-color;
-}
-.leaflet-popup-content-wrapper {
-  background-color: $global-background-color;
-  color: $global-text-color;
-
-  a {
-    color: $global-link-color;
-  }
-}
-.leaflet-popup-tip {
-  background-color: $global-background-color;
 }
 </style>
