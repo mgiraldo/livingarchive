8c6b48b6ce703103a7bdf85fc1a0e9f3407a08f2
Mauricio Giraldo <mauricio@pingpongestudio.com>
2019-06-27 00:58:23 -0400

    :space_invader: added grid view item component

---
+++ components/GridViewItem.vue
@@ -0,0 +1,35 @@
+<template>
+  <li class="grid-item">
+    <h1 class="item-title">{{ individual.individual }}</h1>
+    <bones-find-view :shape="individual.skeleton" />
+  </li>
+</template>
+
+<script>
+import BonesFindView from '~/components/BonesFindView'
+
+export default {
+  components: {
+    BonesFindView
+  },
+  props: {
+    individual: { type: Object, required: true }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.grid-item {
+  height: 10rem;
+  // margin: 0.5rem;
+  position: relative;
+  width: 10rem;
+}
+.item-title {
+  font-size: 1rem;
+  font-weight: normal;
+  left: 0.2rem;
+  position: absolute;
+  top: 0.2rem;
+}
+</style>
