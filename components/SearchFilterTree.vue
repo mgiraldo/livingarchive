dcdb6cdbe988a834e4494c52428b12d5c61a9bbf
Mauricio Giraldo <mauricio@pingpongestudio.com>
2019-07-08 17:49:23 -0400

    :space_invader: started with tree parsing

--- components/SearchFilterTree.vue
+++ components/SearchFilterTree.vue
@@ -33,6 +33,34 @@ export default {
     return { open: false }
   },
   computed: {
+    aggregationTree() {
+      let treeArray = []
+      for (let item in this.sortedAggregations) {
+        const agg = this.sortedAggregations[item]
+        let node = { ...agg }
+        const nodeArray = agg.sortableName.split('.')
+        const nameArray = agg.name.split('.')
+        node.sortableName = nodeArray[0]
+        if (nodeArray.length > 1)
+          node.children = this.parseNode(
+            nodeArray.slice(1),
+            nameArray.slice(1),
+            agg.value
+          )
+        treeArray.push(node)
+      }
+      let finalArray = {}
+      treeArray.forEach(element => {
+        finalArray[element.sortableName] = { ...element }
+        if (element.children) {
+          finalArray[element.sortableName].children = this.putChildren(
+            {},
+            element.children
+          )
+        }
+      })
+      return finalArray
+    },
     sortedAggregations() {
       let sorted = []
       for (let agg in this.aggregations) {
@@ -47,6 +75,30 @@ export default {
     }
   },
   methods: {
+    putChildren(node, children) {
+      children.forEach(element => {
+        node[element.sortableName] = { ...element }
+        if (element.children) {
+          node[element.sortableName].children = this.putChildren(
+            {},
+            element.children
+          )
+        }
+      })
+    },
+    parseNode(nodeArray, nameArray, value) {
+      const sortableName = nodeArray[0]
+      const name = nameArray[0]
+      if (nodeArray.length <= 1) {
+        return { name: name, value: value, sortableName: sortableName }
+      }
+      return {
+        name: name,
+        value: value,
+        sortableName: sortableName,
+        children: this.parseNode(nodeArray.slice(1), nameArray.slice(1), value)
+      }
+    },
     toggle() {
       this.open = !this.open
     }
