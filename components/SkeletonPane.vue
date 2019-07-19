7272b7f90fcfe6c5b74c56dfef7def3181c70fe0
Mauricio Giraldo <mauricio@pingpongestudio.com>
2019-07-19 14:56:28 -0400

    :space_invader: added skeleton pane

---
+++ components/SkeletonPane.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="skeleton">
+    <h1>Bone prevalence</h1>
+    <skeleton-aggregations-viewer
+      :key="$route.fullPath"
+      :aggregations="aggregations"
+    />
+  </div>
+</template>
+
+<script>
+import SkeletonAggregationsViewer from '~/components/SkeletonAggregationsViewer'
+
+export default {
+  components: {
+    SkeletonAggregationsViewer
+  },
+  computed: {
+    aggregations() {
+      return this.$store.state.aggs['bones.bone']
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.skeleton {
+  background-color: lighten($color: $global-background-color, $amount: 3%);
+  flex-basis: 20rem;
+  flex-shrink: 0;
+  overflow: hidden;
+  padding: 0.5rem;
+}
+</style>
