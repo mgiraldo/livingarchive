63a2dafe1970c5dcdca9eca95f14573a91c45c7a
Mauricio Giraldo <mauricio@pingpongestudio.com>
2019-07-24 15:15:59 -0400

    :space_invader: homepage grid retouch

--- pages/index.vue
+++ pages/index.vue
@@ -1,17 +1,18 @@
 <template>
   <section class="container">
     <h1 class="title">Living Archive</h1>
-    <h2 class="subtitle">
-      An experimental interface to the
-      <nuxt-link to="http://catalhoyuk.com/">
-        Çatalhöyük Research Project
-      </nuxt-link>
-      data.
-    </h2>
-    <p class="description">
-      This application was developed to explore the possibilities of visualizing
-      linked data and geospatial data.
-    </p>
+    <div class="extra-content">
+      <h2 class="subtitle">
+        An experimental interface to the
+        <nuxt-link to="http://catalhoyuk.com/">
+          Çatalhöyük Research Project
+        </nuxt-link>
+      </h2>
+      <p class="description">
+        This application was developed to explore the possibilities of
+        visualizing linked data and geospatial data.
+      </p>
+    </div>
     <div class="screenshot-container">
       <nuxt-link to="/map" class="button"
         ><img
@@ -37,7 +38,7 @@ export default {
   display: grid;
   margin: 2rem auto auto;
   grid-template-columns: 1fr 1fr 1fr 6rem 6rem 0.5fr;
-  grid-template-rows: 1fr 5rem 1fr 1fr 0.5fr 1fr;
+  grid-template-rows: 1fr 5rem 1fr auto auto;
   grid-gap: 1rem;
   width: 60vw;
 }
@@ -50,10 +51,14 @@ export default {
   grid-row: 4/5;
 }
 
-.subtitle {
-  font-weight: normal;
+.extra-content {
   grid-column: 3/6;
   grid-row: 5/6;
+}
+
+.subtitle {
+  font-weight: normal;
+  margin-bottom: 1rem;
 }
 
 .screenshot-container {
@@ -75,8 +80,6 @@ export default {
 
 .description {
   font-size: 1rem;
-  grid-column: 3/5;
-  grid-row: 6/7;
 }
 
 .button {
